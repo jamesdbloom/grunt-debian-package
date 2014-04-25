2f38949ae13d3d754015d26ca012070a40fcccdb
James Bloom <james.bloom@nektan.com>
2014-04-25 19:06:06 +0100

    multiple tweaks and improvements

--- tasks/debian_package.js
+++ tasks/debian_package.js
@@ -10,6 +10,9 @@
 
 module.exports = function (grunt) {
 
+    var fs = require('fs'),
+        path = require('path');
+
 
     grunt.registerMultiTask('debian_package', 'Create debian package from grunt build', function () {
         // merge task-specific and target-specific options with defaults
@@ -23,9 +26,12 @@ module.exports = function (grunt) {
             spawn = require('child_process').spawn,
             dateFormat = require('dateformat'),
             replace = require("replace"),
-            now = dateFormat(new Date(), "ddd, dS mmmm yyyy, h:MM:ss +0000"),
+            now = dateFormat(new Date(), "ddd, d mmm yyyy h:MM:ss +0000"),
             changelog = 'packaging/debian/changelog',
             control = 'packaging/debian/control';
+
+        deleteFolderRecursive('packaging');
+        recursiveCopy(__dirname + '/../packaging', 'packaging');
 
         grunt.verbose.writeln('Replacing \'' + now + '\' for ${date} in ' + changelog);
         replace({
@@ -65,15 +71,64 @@ module.exports = function (grunt) {
 
         createTar(this.files, options.name + '_' + options.version + '-' + options.build_number + '.tar');
 
-        grunt.log.writeln("Running debuild...");
-        var debuild = spawn('debuild', ['-i', '-us', '-uc', '-b']);
-        debuild.stdout.on('data', function (data) {
-            grunt.verbose.writeln(data);
-        });
-        debuild.stderr.on('data', function (data) {
-            grunt.log.warn(data);
-        });
+        if (fs.existsSync('/usr/bin/debuild')) {
+            debugger;
+            process.chdir('packaging');
+            var debuild = spawn('debuild', ['-i', '-us', '-uc', '-b']);
+            debuild.stdout.on('data', function (data) {
+                grunt.verbose.writeln(data);
+            });
+            debuild.stderr.on('data', function (data) {
+                grunt.log.warn(data);
+            });
+            debuild.on('close', function (code) {
+                if (code !== 0) {
+                    grunt.log.warn('debuild process exited with code ' + code);
+                } else {
+                    grunt.log.writeln("Successfully ran debuild...");
+                }
+            });
+
+            require('sleep').sleep(5);
+        } else {
+            grunt.log.warn('debuild not found!!');
+            grunt.log.warn('to install debuild try running \'sudo apt-get install devscripts\'');
+            return false;
+        }
     });
+
+    var deleteFolderRecursive = function (path) {
+        var files = [];
+        if (fs.existsSync(path)) {
+            files = fs.readdirSync(path);
+            files.forEach(function (file, index) {
+                var curPath = path + "/" + file;
+                if (fs.lstatSync(curPath).isDirectory()) { // recurse
+                    deleteFolderRecursive(curPath);
+                } else { // delete file
+                    fs.unlinkSync(curPath);
+                }
+            });
+            fs.rmdirSync(path);
+        }
+    };
+
+    var recursiveCopy = function (src, dest) {
+        var exists = fs.existsSync(src),
+            stats = exists && fs.statSync(src),
+            isDirectory = exists && stats.isDirectory();
+
+        if (exists && isDirectory) {
+            fs.mkdirSync(dest);
+            fs.readdirSync(src).forEach(function (childItemName) {
+                recursiveCopy(path.join(src, childItemName), path.join(dest, childItemName));
+            });
+        } else {
+            require('child_process').spawn('cp', [src, dest]);
+            // for an unkown reason fs.linkSync causes an EPERM error (only on vagrant) so resorting to using cp as a work around
+            // fs.linkSync(src, dest);
+        }
+    };
 
     var createTar = function (files, tarFileName) {
         var tarArguments = ['-zcvf', tarFileName];
